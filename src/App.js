a01bd1c941b068359dcc4886ea0cb6a9f156463b
Manning Hitchings <mayhitchings@gmail.com>
2023-09-16 23:03:52 -0400

    connecting the app to the root

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//using ES6 here
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                This is the component.
+            </div>
+        )
+    }
+}
+
+//would be export const if multiple
+export default App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+
+//first what we're inserting, second where we're inserting
+ReactDOM.render( <App/>, document.getElementById('root') );
