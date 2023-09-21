e5bf706254a17a0eb831a61a5b1686781a0336fd
Manning Hitchings <mayhitchings@gmail.com>
2023-09-21 08:47:07 -0400

    adding functionality with button component

--- react-app-webpack/src/App.js
+++ react-app-webpack/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 //using ES6 here
 
+
 class App extends React.Component {
+    constructor ( props ) {
+        super( props )
+    
+        this.state = {
+            name: ''
+        }
+    }
+    
+    handleButtonClickEvent = ( event ) => {
+        //"this" points to class App
+        this.setState( { name: 'Marm' });
+    };
+
     render() {
         return(
             <div>
-                This is the component.
+                <h1>This is the component.</h1>
+                <button onClick={this.handleButtonClickEvent}>Click me</button>
+                { this.state.name && <p>{this.state.name}</p>}
             </div>
         )
     }
