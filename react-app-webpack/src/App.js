3677d9e7fdc6bb80ab750abeaa4291f0a4bedf6a
Manning Hitchings <mayhitchings@gmail.com>
2023-09-21 09:20:46 -0400

    troubles done been shot

--- react-app-webpack/src/App.js
+++ react-app-webpack/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import Lydia from './images/headshot.png';
 //using ES6 here
 
 
@@ -23,6 +24,7 @@ class App extends React.Component {
                 <h1>This is the component.</h1>
                 <button onClick={this.handleButtonClickEvent}>Click me</button>
                 { this.state.name && <p>{this.state.name}</p> }
+                <img src={Lydia} alt="Lydia image"/>
             </div>
         )
     }
--- react-app-webpack/webpack.config.js
+++ react-app-webpack/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
                 use: [ 'style-loader', 'css-loader' ]
             },
             {
-                test: /\.(png|j?g|svg|gif)?$/,
+                test: /\.(j?g|png|svg|gif)?$/,
                 use: 'file-loader'
             }
         ]
