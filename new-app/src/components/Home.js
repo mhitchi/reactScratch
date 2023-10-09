37361330ac651f951e70a3e6ae6387231db26436
Manning Hitchings <mayhitchings@gmail.com>
2023-10-09 13:57:26 -0400

    playing iwth render method

--- new-app/src/components/Home.js
+++ new-app/src/components/Home.js
@@ -1,37 +1,51 @@
 import React from 'react';
 class Home extends React.Component {
 
+    // constructor( props ) {
+    //     super( props );
+
+    //     this.state = {
+    //         number: 0
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     //since no API, setinterval
+    //     this.myInterval =  setInterval( () => {
+    //         this.setState({ number: 0 })
+    //     }, 1000)
+    // }
+
+    // shouldComponentUpdate( nextProps, nextState ) {
+    //     console.warn( nextProps, nextState)
+    //     if ( nextState.number === this.state.number ) {
+    //         return false;
+    //     } else {
+    //         return true;
+    //     }
+    // }
+
+    // componentWillUnmount() {
+    //     clearInterval( this.myInterval );
+    // }
+
     constructor( props ) {
         super( props );
 
         this.state = {
-            number: 0
+            name: ''
         }
     }
 
-    componentDidMount() {
-        //since no API, setinterval
-        this.myInterval =  setInterval( () => {
-            this.setState({ number: 0 })
-        }, 1000)
-    }
-
-    shouldComponentUpdate( nextProps, nextState ) {
-        if ( nextState.number === this.state.number ) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    componentWillUnmount() {
-        clearInterval( this.myInterval );
-    }
+    handleOnClick = () => {
+        this.setState( { name: 'Doodle ' })
+    };
 
     render() {
+        console.warn( 'rendered', this.props)
         return(
             <div>
-                This is a component.
+                <button onClick={this.handleOnClick}>Click Over Here</button>
             </div>
         )
     }
